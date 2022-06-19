e2cda14a2be389e50d53c62e876cbd10b40bfe38
InnitTho <1lamargoesoof@gmail.com>
2022-06-19 11:19:53 +0400

    added cooldown

--- src/main/java/me/saeko/taser_plugin/ListenerOrListeners/Interact.java
+++ src/main/java/me/saeko/taser_plugin/ListenerOrListeners/Interact.java
@@ -1,5 +1,7 @@
 package me.saeko.taser_plugin.ListenerOrListeners;
 
+import me.saeko.taser_plugin.Cooldown;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -19,13 +21,26 @@ public class Interact implements Listener {
                 if (getTargetPlayer(p) != null) {
                     Player targetP = getTargetPlayer(p);
                 double distance = p.getLocation().distance(targetP.getLocation());
-                if (p.getInventory().getItemInMainHand().getType().equals(Material.CARROT_ON_A_STICK)) {
-                    if (distance <= 5) {
+                if (p.getInventory().getItemInMainHand().getType().equals(Material.CARROT_ON_A_STICK)) { //checks if the player is holding a carrot on a stick
+
+                    //This is where the fun begins aka cooldown shit
+                    if(Cooldown.checkCooldown(e.getPlayer())) {
+                        e.getPlayer().sendMessage(ChatColor.DARK_RED + "Taser is now on cooldown for 10 seconds");
+                        Cooldown.setCooldown(e.getPlayer(), 10);
+
+
+
+                    if (distance <= 5) { //makes sure the distance from player A to player B is 5 blocks
+
+                        //The effects that are gonna affect the tased player
                         targetP.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 250));
                         targetP.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 250));
                         targetP.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 100, 250));
 
-
+                        //this part doesnt do shit but at this point im too afraid to try cause it works anyway
+                    } else {
+                        e.getPlayer().sendMessage(ChatColor.DARK_RED + "You cant use this for 10 seconds");
+                        }
                     }
                 }
             }
